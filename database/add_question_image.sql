9973fd0552c841076e10a31b41faca0e3a371ed8
langderen <93062809+langderen@users.noreply.github.com>
2026-02-23 20:52:01 +0800

    完善数据库

--- database/add_question_image.sql
+++
@@ -1,8 +0,0 @@
--- 添加题目图片支持
-USE exercise_system;
-
--- 添加题目图片字段
-ALTER TABLE question ADD COLUMN IF NOT EXISTS content_image VARCHAR(500) DEFAULT NULL COMMENT '题目图片URL' AFTER content;
-ALTER TABLE question ADD COLUMN IF NOT EXISTS options_image VARCHAR(500) DEFAULT NULL COMMENT '选项图片URL' AFTER options;
-
-SELECT '=== 题目图片支持添加完成 ===' as info;
